e3492a8206808f72df21ebbdbb324d9147854cab
minerva-jupiter <ryouturn@gmail.com>
2025-09-10 21:38:29 +0900

    format after add mapid

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,14 @@ export default function Home() {
   return (
     <div>
       <main>
-        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
+        <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ""}>
           <Map
             style={{ width: "100vw", height: "95vh" }}
             defaultCenter={{ lat: 35.486404, lng: 133.06863 }}
             defaultZoom={17}
             gestureHandling={"greedy"}
             disableDefaultUI={true}
-			mapId={process.env.NEXT_PUBLIC_MAP_ID || ''}
+            mapId={process.env.NEXT_PUBLIC_MAP_ID || ""}
           />
         </APIProvider>
       </main>
